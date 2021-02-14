f6c5d2306fe590c2b2368937a92a79d68e2590a9
Ivan Yeromenko <ae300ai@gmail.com>
2021-02-14 16:30:46 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: __dirname + '/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html',
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'create-event.html',
+            template: './src/create-event.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: "styles.css",
+        }),
+        new CleanWebpackPlugin({path:'./dist/*.*'}),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorPluginOptions: {
+                preset: ['default', {
+                    discardComments: {
+                        removeAll: true
+                    }
+                }]
+            },
+            canPrint: true
+        })
+    ]
+};
